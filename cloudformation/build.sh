beb7893baacc628535a0d1254b0cf432c7c8a994
Martin Keßler <martin.kessler@mhp.com>
2020-12-17 15:14:30 +0100

    Replace Explode macro

--- cloudformation/build.sh
+++ cloudformation/build.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]; then
+  echo "Provide environment type as parameter: dev / staging / prod"
+  exit 0
+fi
+
+project_name="mk999"
+stack_name="$project_name-$1-stack"
+
+echo "Building $stack_name..."
 aws cloudformation create-stack \
-  --stack-name myteststack \
+  --stack-name "$stack_name" \
   --template-body file://environments.yml \
   --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND \
-  --parameters ParameterKey=ProjectName,ParameterValue=mk999test
+  --parameters \
+      ParameterKey=ProjectName,ParameterValue="$project_name" \
+      ParameterKey=EnvironmentType,ParameterValue="$1"
--- cloudformation/destroy.sh
+++ cloudformation/destroy.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-aws cloudformation delete-stack --stack-name myteststack
+if [ -z "$1" ]; then
+  echo "Provide environment type as parameter: dev / staging / prod"
+  exit 0
+fi
 
+project_name="mk999"
+stack_name="$project_name-$1-stack"
+
+echo "Destroying $stack_name..."
+aws cloudformation delete-stack --stack-name "$stack_name"
